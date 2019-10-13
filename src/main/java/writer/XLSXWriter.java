72fac385a7c1c1f5a235962aa07a8583c71a509a
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-13 19:59:47 -0400

    format file and close io class

--- src/main/java/writer/XLSXWriter.java
+++ src/main/java/writer/XLSXWriter.java
@@ -11,60 +11,61 @@ import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 /**
- *
  * @author Cameron
  */
 public class XLSXWriter {
-    
-    /**
-     * Basic constructor for writing a doc
-     * @param list List of US that will be printed out.
-     */
-    public XLSXWriter(List<UserStoryXLSX> list) {
-        // create document
-        XWPFDocument document = new XWPFDocument();
-       
-        // create head of document and write to file
-        XWPFParagraph tmpParagraph = document.createParagraph();
-        XWPFRun tmpRun = tmpParagraph.createRun();
-        tmpRun.setText("CA Agile Central extracted Data");
-        tmpRun.setFontSize(18);
-        try {
-                document.write(new FileOutputStream(new File("yourTestHere.docx")));
-        } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-        }
-        System.out.print("AAA");
-        
-        //loop through list of US and write each to the file
-        Iterator<UserStoryXLSX> iterator = list.iterator();
-        while( iterator.hasNext() ) {
-            System.out.print("AAA");
-            
-            // reses paragraph
-            tmpParagraph = document.createParagraph();
-            tmpRun = tmpParagraph.createRun();
-            
-            // put US data into string
-            UserStoryXLSX us = iterator.next();
-            String usString = "US";
-            usString += us.getId() + ": ";
-            usString += us.getName() + "\n";
-            usString += "Description: " + us.getDescription() + "\n";
-            usString += "Notes: " + us.getNotes() + "\n";
-            
-            // set the US string to be written in to doc
-            tmpRun.setText(usString);
-            tmpRun.setFontSize(12);
-            try {
-                    document.write(new FileOutputStream(new File("yourTestHere.docx")));
-            } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-            }
-        }
-        
-        //document.close();
-    }
+
+	/**
+	 * Basic constructor for writing a doc
+	 * @param list List of US that will be printed out.
+	 */
+	public XLSXWriter(List<UserStoryXLSX> list) {
+		// create variables for writing documents
+		XWPFDocument document;
+		XWPFParagraph tmpParagraph;
+		XWPFRun tmpRun;
+
+		// create first document
+		document = new XWPFDocument();
+		tmpParagraph = document.createParagraph();
+		tmpRun = tmpParagraph.createRun();
+		tmpRun.setText("CA Agile Central extracted Data");
+		tmpRun.setFontSize(18);
+
+		try {
+			// create head of document and write to file
+			document.write(new FileOutputStream(new File("yourTestHere.docx")));
+			document.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		//loop through list of US and write each to the file
+		Iterator<UserStoryXLSX> iterator = list.iterator();
+		while (iterator.hasNext()) {
+
+			// reset writer objects
+			document = new XWPFDocument();
+			tmpParagraph = document.createParagraph();
+			tmpRun = tmpParagraph.createRun();
+
+			// put US data into string
+			UserStoryXLSX us = iterator.next();
+			String usString = "US";
+			usString += us.getId() + ": ";
+			usString += us.getName() + "\n";
+			usString += "Description: " + us.getDescription() + "\n";
+			usString += "Notes: " + us.getNotes() + "\n";
+
+			// set the US string to be written in to doc
+			tmpRun.setText(usString);
+			tmpRun.setFontSize(12);
+			try {
+				document.write(new FileOutputStream(new File("yourTestHere.docx")));
+				document.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
