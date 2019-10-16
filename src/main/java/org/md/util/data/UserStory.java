2216fd4a4320768caae019c750a4ed31b2fde664
Cam Dziurgot <camerondziurgot@gmail.com>
2019-10-15 23:14:56 -0400

    put all model info into single object

--- src/main/java/org/md/util/data/UserStory.java
+++
@@ -1,9 +0,0 @@
-package org.md.util.data;
-
-/**
- * Class used to define all the methods needed for a basic User Story object.
- * Also adds all the variables the class requires.
- * @author Cameron
- *
- */
-public abstract class UserStory extends AbstractAgileObject { }
--- src/main/java/org/md/util/data/UserStoryXLSX.java
+++ src/main/java/org/md/util/data/UserStoryXLSX.java
@@ -5,7 +5,16 @@ package org.md.util.data;
  * @author Cameron
  *
  */
-public class UserStoryXLSX extends UserStory {
+public class UserStoryXLSX {
+
+	private String description;
+	private String discussions;
+	private Integer id;
+	private String notes;
+	private String name;
+	private String source;
+	private String status;
+	private String type;
 
 	/**
 	 * User story formatted ID code number.
@@ -13,5 +22,142 @@ public class UserStoryXLSX extends UserStory {
 	 */
 	public UserStoryXLSX(int id) {
 		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDiscussions() {
+		return discussions;
+	}
+
+	public void setDiscussions(String discussions) {
+		this.discussions = discussions;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "UserStoryXLSX [description=" + description + ", discussions=" + discussions + ", id=" + id + ", notes="
+				+ notes + ", name=" + name + ", source=" + source + ", status=" + status + ", type=" + type + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((discussions == null) ? 0 : discussions.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((notes == null) ? 0 : notes.hashCode());
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserStoryXLSX other = (UserStoryXLSX) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (discussions == null) {
+			if (other.discussions != null)
+				return false;
+		} else if (!discussions.equals(other.discussions))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (notes == null) {
+			if (other.notes != null)
+				return false;
+		} else if (!notes.equals(other.notes))
+			return false;
+		if (source == null) {
+			if (other.source != null)
+				return false;
+		} else if (!source.equals(other.source))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
 	}
 }
